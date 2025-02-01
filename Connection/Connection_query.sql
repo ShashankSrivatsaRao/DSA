abf187c9b58d20045c1609fe5ab3092925ec5df3
ShashankSrivatsaRao <1rn21is141.shashank@gmail.com>
2025-02-01 13:47:52 +0530

    Added SQL Script for connection

---
+++ Connection/Connection_query.sql
@@ -0,0 +1,24 @@
+/*drop table if exists Obesity
+
+CREATE TABLE dbo.Obesity
+(
+    Gender varchar(100) ,
+	Age float,
+	Height float,
+	Weight float ,
+	family_history varchar(20),
+	FAVC varchar(100),
+	FCVC float,
+	NCP float ,
+	CAEC varchar(100),
+	SMOKE varchar(100),
+	CH2O float ,
+	SCC varchar(100),
+	FAF float,
+	TUE float,
+	CALC varchar(100),
+	MTRANS varchar(100),
+	Obesity varchar(100)
+);*/
+
+select * from Obesity
